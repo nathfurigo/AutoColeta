7a76c62e9a469782efe3c3b96e69c305d5d03453
nathfurigo <nathalia.furigo@tecnolog.com.br>
2025-10-10 11:48:24 -0300

    Ajuste horário

--- src/main/java/com/tecnolog/autocoleta/AutocoletaApplication.java
+++ src/main/java/com/tecnolog/autocoleta/AutocoletaApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 @EnableFeignClients
 @EnableConfigurationProperties(AppProperties.class)
 public class AutocoletaApplication {
